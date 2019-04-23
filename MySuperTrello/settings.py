66edfa76a86914dc1a57b9b5c334835b0e57d31b
Vladimir Kurbatov <warkb@yandex.ru>
2019-04-23 10:22:11 +0400

    Добавил модели карточек и досок

--- MySuperTrello/settings.py
+++ MySuperTrello/settings.py
@@ -107,7 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
+
+USE_I18N = True
 
 TIME_ZONE = 'UTC'
 
--- mySuperTrelloApp/models.py
+++ mySuperTrelloApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# логин: admin
+# пароль: trelloadmin
+
+class Desc(models.Model):
+    name = models.CharField(max_length=200, help_text='Имя доски')
+    class Meta:
+        verbose_name = 'Доска'
+        verbose_name_plural = 'Доски'
+
+class Card(models.Model):
+    text = models.TextField(help_text='Позиция на доске')
+    data = models.DateTimeField(auto_now_add=True, help_text='Время создания')
+    desc = models.ForeignKey(Desc, on_delete=models.CASCADE, help_text='Родительская доска')
+    position = models.PositiveIntegerField(help_text='Позиция на доске')
+    class Meta:
+        verbose_name = 'Карточка'
+        verbose_name_plural = 'Карточки'
